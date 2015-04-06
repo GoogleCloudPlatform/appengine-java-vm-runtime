8a3ae137251fee915fe280830a33ce128a4b1940
ludoch <ludovic.champenois@gmail.com>
2015-04-06 04:59:29 -0700

    build script

---
+++ buildimage.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+mvn clean install
+rm -rf docker/lib/ext
+mkdir -p docker/lib/ext
+cp appengine-jetty-managed-runtime/target/appengine-jetty-managed-runtime-*.jar docker/lib/ext/appengine-jetty-managed-runtime.jar
+cp appengine-managed-runtime/target/appengine-managed-runtime-*.jar docker/lib/ext/appengine-managed-runtime.jar
+cd docker
+output=${1:-appengine-mvn-opensource}
+echo using $output as the image name [you can overwrite it by passing the desired name in the command]
+docker build -t appengine-mvn-opensource .
+
+
