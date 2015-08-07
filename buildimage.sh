eeac4b9aea5cc42c313a82d78874bea0a3a22f16
Greg Wilkins <gregw@webtide.com>
2015-08-07 15:31:49 +1000

    Issue 25 - Generate jetty_cmd.sh

--- buildimage.sh
+++ buildimage.sh
@@ -5,6 +5,7 @@ mkdir -p docker/lib/ext
 cp appengine-jetty-managed-runtime/target/jetty-distribution.tar.gz docker/
 cp appengine-jetty-managed-runtime/target/appengine-jetty-managed-runtime-*.jar docker/lib/ext/appengine-jetty-managed-runtime.jar
 cp appengine-managed-runtime/target/appengine-managed-runtime-*.jar docker/lib/ext/appengine-managed-runtime.jar
+cp appengine-managed-runtime/target/appengine-api-1.0-sdk-*.jar docker/lib/ext/appengine-api-1.0-sdk.jar
 cd docker
 output=${1:-appengine-mvn-opensource}
 echo using $output as the image name [you can overwrite it by passing the desired name in the command]
