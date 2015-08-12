ff54f587c1c8f90f915f83ba457efef84435d031
Greg Wilkins <gregw@webtide.com>
2015-08-13 09:34:47 +1000

    Issue-25 unpack in build sh

--- buildimage.sh
+++ buildimage.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 mvn clean install
+
+rm -rf docker/jetty
+mkdir -p docker/jetty
+tar -C docker/jetty --strip-components=1 -zxf appengine-jetty-managed-runtime/target/jetty-distribution.tar.gz
+
 rm -rf docker/lib/ext
 mkdir -p docker/lib/ext
-cp appengine-jetty-managed-runtime/target/jetty-distribution.tar.gz docker/
 cp appengine-jetty-managed-runtime/target/appengine-jetty-managed-runtime-*.jar docker/lib/ext/appengine-jetty-managed-runtime.jar
 cp appengine-managed-runtime/target/appengine-managed-runtime-*.jar docker/lib/ext/appengine-managed-runtime.jar
 cp appengine-managed-runtime/target/appengine-api-1.0-sdk-*.jar docker/lib/ext/appengine-api-1.0-sdk.jar
