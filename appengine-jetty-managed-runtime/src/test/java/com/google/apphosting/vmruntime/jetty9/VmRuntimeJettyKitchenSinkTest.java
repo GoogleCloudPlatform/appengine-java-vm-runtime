c609ccfa2c64c663adfc848ca08dba9d25f00a33
ludoch <ludovic.champenois@gmail.com>
2015-08-19 14:22:39 -0700

    More unit test changes.

--- appengine-jetty-managed-runtime/src/test/java/com/google/apphosting/vmruntime/jetty9/VmRuntimeJettyKitchenSinkTest.java
+++ appengine-jetty-managed-runtime/src/test/java/com/google/apphosting/vmruntime/jetty9/VmRuntimeJettyKitchenSinkTest.java
@@ -1,19 +1,18 @@
 /**
  * Copyright 2015 Google Inc. All Rights Reserved.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS-IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.google.apphosting.vmruntime.jetty9;
 
 import com.google.apphosting.api.ApiProxy;
@@ -25,7 +24,7 @@ import java.util.Arrays;
  * Misc individual Jetty9 vmengines tests.
  *
  */
-public class VmRuntimeJetty9KitchenSinkTest extends VmRuntimeTestBase {
+public class VmRuntimeJettyKitchenSinkTest extends VmRuntimeTestBase {
 
   /**
    * Test that non compiled jsp files can be served.
@@ -35,8 +34,8 @@ public class VmRuntimeJetty9KitchenSinkTest extends VmRuntimeTestBase {
   public void testJspNotCompiled() throws Exception {
     int iter = 0;
     int end = 6;
-    String[] lines =
-        fetchUrl(createUrl(String.format("/hello_not_compiled.jsp?start=%d&end=%d", iter, end)));
+    String[] lines
+            = fetchUrl(createUrl(String.format("/hello_not_compiled.jsp?start=%d&end=%d", iter, end)));
     String iterationFormat = "<h2>Iteration %d</h2>";
     for (String line : lines) {
       System.out.println(line);
@@ -66,7 +65,7 @@ public class VmRuntimeJetty9KitchenSinkTest extends VmRuntimeTestBase {
   public void testApiProxyInstall() throws Exception {
     assertNotNull(ApiProxy.getDelegate());
     assertEquals(VmApiProxyDelegate.class.getCanonicalName(),
-        ApiProxy.getDelegate().getClass().getCanonicalName());
+            ApiProxy.getDelegate().getClass().getCanonicalName());
   }
 
 }
