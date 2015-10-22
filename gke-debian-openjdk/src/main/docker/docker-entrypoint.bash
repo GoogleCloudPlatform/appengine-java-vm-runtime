78cb31cf817c01e26219cbb139a2e1ca7e6b72a4
Greg Wilkins <gregw@webtide.com>
2015-10-22 15:07:05 +1100

    Added support for JAVA_OPTS

--- gke-debian-openjdk/src/main/docker/docker-entrypoint.bash
+++ gke-debian-openjdk/src/main/docker/docker-entrypoint.bash
@@ -6,7 +6,7 @@ fi
 
 if ! type "$1" &>/dev/null; then
   source /gke-env.bash
-  set -- java $ALPN_BOOT $DBG_AGENT $PROF_AGENT $SET_TMP "$@"
+  set -- java $JAVA_OPTS $ALPN_BOOT $DBG_AGENT $PROF_AGENT $SET_TMP "$@"
 fi
 
 exec "$@"
