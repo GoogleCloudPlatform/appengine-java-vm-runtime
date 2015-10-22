0b99ece0b8b1fb573b01cf1c414415bc5efc565f
Greg Wilkins <gregw@webtide.com>
2015-10-22 14:29:33 +1100

    Refined entry point and added gke-env.bash for #117 / #118

--- gke-debian-openjdk/src/main/docker/docker-entrypoint.bash
+++ gke-debian-openjdk/src/main/docker/docker-entrypoint.bash
@@ -1,34 +1,11 @@
 #!/bin/bash
 
+if [ "java" = "$1" -o "$(which java)" = "$1" ] ; then
+  shift
+fi
+
 if ! type "$1" &>/dev/null; then
-  ALPN_BOOT=
-  if [[ -n "$ALPN_ENABLE" ]]; then
-    ALPN_BOOT="$( /opt/alpn/format-env-appengine-vm.sh )"
-  fi
-
-  DBG_AGENT=
-  if [[ -n "$DBG_ENABLE" ]]; then
-    if [[ "$GAE_PARTITION" = "dev" ]]; then
-      DBG_AGENT="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=${DBG_PORT:-5005}"
-    else
-      DBG_AGENT="$( /home/vmagent/cdbg/format-env-appengine-vm.sh )"
-    fi
-  fi
-
-  PROF_AGENT=
-  if [[ -n "${CPROF_ENABLE}" ]]; then
-    if [[ "$GAE_PARTITION" = "dev" ]]; then
-      PROF_AGENT=
-    else
-      PROF_AGENT="$( /home/vmagent/cprof/format-env-appengine-vm.sh )"
-    fi
-  fi
-
-  SET_TMP=
-  if [[ -n "${TMPDIR}" ]]; then
-    SET_TMP="-Djava.io.tmpdir=$TMPDIR"
-  fi
-
+  source /gke-env.bash
   set -- java $ALPN_BOOT $DBG_AGENT $PROF_AGENT $SET_TMP "$@"
 fi
 
---
+++ gke-debian-openjdk/src/main/docker/gke-env.bash
@@ -0,0 +1,29 @@
+#!/bin/bash
+ALPN_BOOT=
+if [[ -n "$ALPN_ENABLE" ]]; then
+  ALPN_BOOT="$( /opt/alpn/format-env-appengine-vm.sh )"
+fi
+
+DBG_AGENT=
+if [[ -n "$DBG_ENABLE" ]]; then
+  if [[ "$GAE_PARTITION" = "dev" ]]; then
+    DBG_AGENT="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=${DBG_PORT:-5005}"
+  else
+    DBG_AGENT="$( /home/vmagent/cdbg/format-env-appengine-vm.sh )"
+  fi
+fi
+
+PROF_AGENT=
+if [[ -n "${CPROF_ENABLE}" ]]; then
+  if [[ "$GAE_PARTITION" = "dev" ]]; then
+    PROF_AGENT=
+  else
+    PROF_AGENT="$( /home/vmagent/cprof/format-env-appengine-vm.sh )"
+  fi
+fi
+
+SET_TMP=
+if [[ -n "${TMPDIR}" ]]; then
+  SET_TMP="-Djava.io.tmpdir=$TMPDIR"
+fi
+
