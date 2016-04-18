d05d5d872051a7e4d990c8ed60890fabeab7c51d
Chanseok Oh <chanseokoh@users.noreply.github.com>
2016-04-18 19:04:08 -0400

    Enable Stackdriver Debugger by default (#220)
    * Enable Stackdriver Debugger by default
    
    * Change Cloud Debugger to Stackdriver Debugger

--- gke-debian-openjdk/src/main/docker/gke-env.bash
+++ gke-debian-openjdk/src/main/docker/gke-env.bash
@@ -4,14 +4,12 @@ if [[ -n "$ALPN_ENABLE" ]]; then
   ALPN_BOOT="$( /opt/alpn/format-env-appengine-vm.sh )"
 fi
 
-DBG_AGENT=
+DBG_AGENT="$( /opt/cdbg/format-env-appengine-vm.sh )"
 if [[ -n "$DBG_ENABLE" ]]; then
   if [[ "$GAE_PARTITION" = "dev" ]]; then
     echo "Running locally and DBG_ENABLE is set, enabling standard Java debugger agent"
     DBG_PORT=${DBG_PORT:-5005}
     DBG_AGENT="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=${DBG_PORT}"
-  else
-    DBG_AGENT="$( /opt/cdbg/format-env-appengine-vm.sh )"
   fi
 fi
 
