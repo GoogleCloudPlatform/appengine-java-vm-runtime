c367b6ccd98dc2c307adf3d43585f4322f1caa87
Chanseok Oh <chanseokoh@users.noreply.github.com>
2016-04-26 18:57:21 -0400

    Explicitly unpack root.war (if present) into webapps/root (#222)
    * Explicitly unpack root.war if present:
    
    - Revert last Pull Requst that creates and touches webapps/root
    - Install zip
    - Unpack root.war explicitly if present
    - Set RUNTIME_DIR and pass it to cdbg/format-env-appengine-vm.sh
    
    * Install unzip, not zip
    
    * Unpack root.war only when it is safe
    
    * Trigger CI rebuild (empty comit)
    
    * Trigger CI rebuild (empty commit)

--- gke-jetty/src/main/docker/docker-entrypoint.bash
+++ gke-jetty/src/main/docker/docker-entrypoint.bash
@@ -3,6 +3,18 @@
 # default jetty arguments
 DEFAULT_ARGS="-Djetty.base=$JETTY_BASE -jar $JETTY_HOME/start.jar"
 
+# Unpack a WAR app (if present) beforehand so that Stackdriver Debugger
+# can load it. This should be done before the JVM for Jetty starts up.
+WEBAPP_WAR=$JETTY_BASE/webapps/root.war
+WEBAPP_ROOT=$JETTY_BASE/webapps/root
+if [ -e "$WEBAPP_WAR" ]; then
+  # Unpack it only if $WEBAPP_ROOT doesn't exist or empty.
+  if [ ! -e "$WEBAPP_ROOT" -o ! "$( ls -A $WEBAPP_ROOT )" ]; then
+    unzip $WEBAPP_WAR -d $WEBAPP_ROOT
+    chown -R jetty.jetty $WEBAPP_ROOT
+  fi
+fi
+
 # If the passed arguments start with the java command
 if [ "java" = "$1" -o "$(which java)" = "$1" ] ; then
   # ignore the java command as it is the default
