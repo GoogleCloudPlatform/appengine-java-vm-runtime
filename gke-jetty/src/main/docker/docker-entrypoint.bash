192cb8f30508aaeab4089f45e9a0257b9375fe11
Chanseok Oh <chanseok@google.com>
2016-04-26 11:59:29 -0400

    Unpack root.war only when it is safe

--- gke-jetty/src/main/docker/docker-entrypoint.bash
+++ gke-jetty/src/main/docker/docker-entrypoint.bash
@@ -3,9 +3,16 @@
 # default jetty arguments
 DEFAULT_ARGS="-Djetty.base=$JETTY_BASE -jar $JETTY_HOME/start.jar"
 
-if [ -e "$JETTY_BASE/webapps/root.war" ]; then
-  unzip $JETTY_BASE/webapps/root.war -d $JETTY_BASE/webapps/root
-  chown -R jetty.jetty $JETTY_BASE/webapps/root
+# Unpack a WAR app (if present) beforehand so that Stackdriver Debugger
+# can load it. This should be done before the JVM for Jetty starts up.
+WEBAPP_WAR=$JETTY_BASE/webapps/root.war
+WEBAPP_ROOT=$JETTY_BASE/webapps/root
+if [ -e "$WEBAPP_WAR" ]; then
+  # Unpack it only if $WEBAPP_ROOT doesn't exist or empty.
+  if [ ! -e "$WEBAPP_ROOT" -o ! "$( ls -A $WEBAPP_ROOT )" ]; then
+    unzip $WEBAPP_WAR -d $WEBAPP_ROOT
+    chown -R jetty.jetty $WEBAPP_ROOT
+  fi
 fi
 
 # If the passed arguments start with the java command
