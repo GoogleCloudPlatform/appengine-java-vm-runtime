0bdcea00215b628db4172fd87536e15f7c937fe9
Greg Wilkins <gregw@webtide.com>
2016-06-04 10:03:19 +1000

    Issue #267 Enable debug case insensitive
    Signed-off-by: Greg Wilkins <gregw@webtide.com>

--- openjdk8/src/main/docker/setup-env.bash
+++ openjdk8/src/main/docker/setup-env.bash
@@ -1,11 +1,20 @@
 #!/bin/bash
+
+isTrue() {  
+  if [[ ${1,,*} = "true" ]] ;then 
+    return ${true}
+  else 
+    return ${false}
+  fi 
+}
+
 ALPN_BOOT=
-if [[ "${ALPN_ENABLE:=false}" = "true" ]]; then
+if isTrue "${ALPN_ENABLE:=false}" ; then
   ALPN_BOOT="$( /opt/alpn/format-env-appengine-vm.sh )"
 fi
 
 DBG_AGENT=
-if [[ "${DBG_ENABLE:=$( if [[ -z ${CDBG_DISABLE} ]] ; then echo true; else echo false ; fi )}" = "true" ]]; then
+if isTrue "${DBG_ENABLE:=$( if [[ -z ${CDBG_DISABLE} ]] ; then echo true; else echo false ; fi )}" ; then
   if [[ "$GAE_PARTITION" = "dev" ]]; then
     echo "Running locally and DBG_ENABLE is set, enabling standard Java debugger agent"
     DBG_AGENT="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=${DBG_PORT:=5005}"
