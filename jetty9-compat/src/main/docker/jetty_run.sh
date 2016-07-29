4d3232e273a78785132d25616582ccbd9e8d8394
ludovic Champenois <ludovic.champenois@gmail.com>
2016-07-29 13:28:44 -0700

    Fix https://github.com/GoogleCloudPlatform/appengine-java-vm-runtime/issues/308

--- jetty9-compat/src/main/docker/jetty_run.sh
+++ jetty9-compat/src/main/docker/jetty_run.sh
@@ -17,4 +17,6 @@ source /setup-env.bash
   
 # use generated fast cli:
 cd /var/lib/jetty
+# TODO remove when we upgrade to a newer GAE staging phase, see issue #308
+sed -i -e '/^    "jar:file:/d' /app/WEB-INF/quickstart-web.xml
 source /var/lib/jetty/jetty_cmd.sh
