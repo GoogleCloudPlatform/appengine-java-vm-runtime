082a4076fa16c2f326b8005a94c844c26b57f652
Greg Wilkins <gregw@webtide.com>
2016-11-22 11:01:23 +1100

    Issue #333 Simplified Async FileHandler

--- appengine-managed-runtime/src/main/java/com/google/apphosting/vmruntime/VmRuntimeFileLogHandler.java
+++ appengine-managed-runtime/src/main/java/com/google/apphosting/vmruntime/VmRuntimeFileLogHandler.java
@@ -22,7 +22,6 @@ import java.io.IOException;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.FileHandler;
 import java.util.logging.Handler;
 import java.util.logging.Level;
@@ -49,16 +48,15 @@ public class VmRuntimeFileLogHandler extends FileHandler {
   private static final int LOG_MAX_FILES = 3;
   public static final String JAVA_UTIL_LOGGING_CONFIG_PROPERTY = "java.util.logging.config.file";
   
-  private static final AtomicInteger handlers = new AtomicInteger();
-  private static final BlockingQueue<LogEntry> queue = new LinkedBlockingQueue<>();
+  private static final LogRecord CLOSE_RECORD = new LogRecord(Level.ALL, "CLOSE");
+  private final BlockingQueue<LogRecord> queue = new LinkedBlockingQueue<>();
+  
   
   private VmRuntimeFileLogHandler() throws IOException {
     super(fileLogPattern(), LOG_MAX_SIZE, LOG_MAX_FILES, true);
     setLevel(Level.FINEST);
     setFormatter(new JsonFormatter());
-    if (handlers.getAndIncrement() == 0 ) {
-      new LoggerThread().start();
-    }
+    new LoggerThread().start();
   }
 
   private static String fileLogPattern() {
@@ -115,51 +113,33 @@ public class VmRuntimeFileLogHandler extends FileHandler {
   @Override
   public void close() throws SecurityException {
     super.close();
-    if (handlers.decrementAndGet() <= 0 ) {
-      queue.offer(null);
-    }
+    queue.offer(CLOSE_RECORD);
   }
 
   @Override
   public void publish(LogRecord record) {
     if (isLoggable(record)) {
-      queue.offer(new LogEntry(record));
+      queue.offer(record);
     }
   }
 
   private void superPublish(LogRecord record) {
     super.publish(record);
   }
-
-  private class LogEntry {
-    final LogRecord record;
-
-    LogEntry(LogRecord record) {
-      this.record = record;
-    }
-    
-    void publish() {
-      superPublish(record);
-    }
-  }
   
-  static class LoggerThread extends Thread {
+  class LoggerThread extends Thread {
     @Override
     public void run() {
       while (true) {
         try {
-          LogEntry entry = queue.poll(1, TimeUnit.HOURS);
-          if (entry == null) {
-            if (handlers.get() == 0) {
-              return;
-            }
+          LogRecord record = queue.poll(1, TimeUnit.HOURS);
+          if (record == CLOSE_RECORD) {
+            return;
           } else {
-            entry.publish();
+            superPublish(record);
           }
         } catch (InterruptedException e) {
-          if (handlers.get() == 0) {
-            return;
-          }
+          return;
         }
       }
     }
